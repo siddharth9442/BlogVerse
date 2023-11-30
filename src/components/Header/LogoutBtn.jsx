e1cf337a994a0ffa6cfd9c5e03f36426c69c953c
Siddharth <sidkhubikar21@gmail.com>
2023-11-30 13:13:49 +0530

    change in logout

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {useDispatch} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import authService from '../../appwrite/auth'
 import {logout} from '../../store/authSlice'
 
 function LogoutBtn() {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const logoutHandler = () => {
         authService.logout().then(() => {
             dispatch(logout())
+            navigate("/")
         })
     }
   return (
